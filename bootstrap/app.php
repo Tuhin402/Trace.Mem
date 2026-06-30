42e07cda44b901c606fa8a2e458ec0b6b3b82b98
Tuhin402 <tuhinsarkar581@gmail.com>
2026-07-01 03:52:26 +0530

    fixed api routes

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,9 +12,13 @@ use Illuminate\Http\Middleware\HandleCors;
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
-        api: __DIR__.'/../routes/api.php',
+        // api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
+        then: function () {
+            Route::middleware('api')
+                ->group(base_path('routes/api.php'));
+        },
     )
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->encryptCookies(except: ['appearance', 'sidebar_state']);

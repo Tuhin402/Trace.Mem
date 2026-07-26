89f69a3c1f7e9146856d676fd12387022af48486
Tuhin402 <tuhinsarkar581@gmail.com>
2026-07-26 15:08:57 +0530

    fix: rewrite control-otp email to use custom HTML master layout

--- resources/views/emails/auth/control-otp.blade.php
+++ resources/views/emails/auth/control-otp.blade.php
@@ -1,20 +1,49 @@
-<x-mail::message>
-# Operations Console Verification
+@extends('emails.layouts.master')
 
-Hello {{ $data['user_name'] ?? 'Administrator' }},
+@section('content')
 
-A request was made to access the TraceMem Operations Console. Please use the following One-Time Password (OTP) to verify your identity:
+<h1 style="
+    margin: 0 0 8px;
+    font-family: {{ $theme::fontFamily() }};
+    font-size: 24px;
+    font-weight: 700;
+    color: {{ $theme::text() }};
+    letter-spacing: -0.3px;
+    line-height: 1.3;
+">
+    Operations Console Verification
+</h1>
+<p style="
+    margin: 0 0 28px;
+    font-family: {{ $theme::fontFamily() }};
+    font-size: {{ $theme::fontSizeBase() }};
+    color: {{ $theme::textMuted() }};
+    line-height: {{ $theme::lineHeight() }};
+">
+    Hello {{ $user_name ?? 'Administrator' }},<br><br>
+    A request was made to access the TraceMem Operations Console. Please use the following One-Time Password (OTP) to verify your identity:
+</p>
 
-<x-mail::panel>
-<div style="text-align: center; font-size: 32px; font-weight: bold; letter-spacing: 4px; padding: 10px;">
-{{ $data['otp'] }}
+<div style="
+    text-align: center; 
+    font-size: 32px; 
+    font-weight: bold; 
+    letter-spacing: 4px; 
+    padding: 20px; 
+    background-color: {{ $theme::surfaceMuted() }}; 
+    border: 1px solid {{ $theme::border() }}; 
+    color: {{ $theme::primary() }};
+    border-radius: {{ $theme::borderRadius() }};
+    margin-bottom: 28px;
+">
+    {{ $otp }}
 </div>
-</x-mail::panel>
 
-This code is valid for **5 minutes**. 
+@include('emails.components.divider')
 
-If you did not request this code, please ignore this email. Your account remains secure.
+@include('emails.components.alert', [
+    'type'    => 'info',
+    'message' => 'This code is valid for 5 minutes. If you did not request this code, please ignore this email. Your account remains secure.'
+])
 
-Thanks,<br>
-{{ config('app.name') }} Security Operations
-</x-mail::message>
+@endsection
